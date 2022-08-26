94a6522c366549c942b78a675e11fcdb938ea1ae
Albert Jonathan <borjajonathann@gmail.com>
2022-08-26 13:55:38 +0800

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 const router = require('./routes')
 
@@ -36,7 +37,10 @@ app.use(
   session({
     secret:'keyboard cat',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI
+    })
   })
 )
 
