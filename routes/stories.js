ddac983fbfe892982ba18d0b38ec5188881d28c5
Albert Jonathan <borjajonathann@gmail.com>
2022-08-29 07:52:56 +0800

    added last routes and hbs file

--- routes/stories.js
+++ routes/stories.js
@@ -47,9 +47,21 @@ router.get('/', ensureAuth, async (req,res) => {
 // @route     GET /stories/:id
 router.get('/:id', ensureAuth, async (req,res) => {
   try {
-    let story = await Story.findById()
+    let story = await Story.findById(req.params.id)
+    .populate('user')
+    .lean()
+
+    if(!story){
+      return res.render('error/404')
+    }
+
+    res.render('stories/show' , {
+      story
+    })
+
   } catch (err) {
-    
+    console.error(err)
+    res.render('error/404')
   }
 })
 
@@ -107,9 +119,8 @@ router.put('/:id', ensureAuth,  async (req,res) => {
     console.error(err)
     return res.render('error/500')
   }
-
-
 })
+
 
 // @desc      Delete story
 // @route     DELETE /stories/:id
@@ -122,5 +133,28 @@ router.delete('/:id', ensureAuth, async (req,res) => {
     return res.render('error/500')
   }
 })
+
+
+// @desc      User stories
+// @route     GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req,res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public'
+    })
+    .populate('user')
+    .lean()
+
+    res.render('stories/index', {
+      stories
+    })
+
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 
 module.exports = router
