03c3b92a9131cf3a5d47fb8706e8c5cf3502647b
Albert Jonathan <borjajonathann@gmail.com>
2022-08-28 14:37:57 +0800

    new edit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
 
 // Handlebars
 app.engine(
@@ -39,6 +39,7 @@ app.engine(
       formatDate,
       stripTags,
       truncate,
+      editIcon
     },
     defaultLayout: 'main',
     extname: '.hbs'
@@ -61,6 +62,12 @@ app.use(
 // Passport middlware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
