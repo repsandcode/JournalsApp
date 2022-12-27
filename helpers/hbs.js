5f40b59b49f997fee1bb967f0471c93f8786e78d
Albert Jonathan Borja <albertjonathan.dev@gmail.com>
2022-12-27 12:14:28 +0800

    restyled

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -20,9 +20,9 @@ module.exports = {
   editIcon: function (storyUser, loggedUser, storyId, floating = true) {
     if (storyUser._id.toString() == loggedUser._id.toString()) {
       if (floating) {
-        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`
+        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab"><i class="fas fa-edit fa-small bg-edit"></i></a>`
       } else {
-        return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`
+        return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit color-edit"></i></a>`
       }
     } else {
       return ''
